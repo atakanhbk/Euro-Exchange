7377618a60a529e373b676aa3459aae41ebb9c69
atakanhbk <atakanhbk@gmail.com>
2023-09-19 19:23:24 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById("change").addEventListener("click",function () {
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if(this.status === 200){
+            const response = JSON.parse(this.responseText);
+            const rate = response.rates.TRY;
+            const amount = Number(document.getElementById("amount").value);
+            console.log("Test");
+            document.getElementById("tl").value = amount * rate;
+         
+        } 
+      
+    }
+
+    xhr.open("GET","https://api.exchangerate.host/latest",true);
+
+    xhr.send();
+
+  
+})
