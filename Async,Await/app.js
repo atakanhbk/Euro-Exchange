eeb96896cf0c609bc1601b27ba8f27fe9057d2f3
atakanhbk <atakanhbk@gmail.com>
2023-09-28 17:40:25 +0300

    Update app.js

--- Async,Await/app.js
+++ Async,Await/app.js
@@ -12,23 +12,36 @@
 
 // test("Merhaba").then(response => console.log(response));
 
-async function testData(data) {
-    let promise = new Promise((resolve,reject) => {
-        setTimeout(() => {
-            if (typeof data === "string") {
-                resolve("This data is string");
-            }
-            else{
-                reject("Please enter string value");
-            }
-        },5000)
-    })
+// async function testData(data) {
+//     let promise = new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             if (typeof data === "string") {
+//                 resolve("This data is string");
+//             }
+//             else{
+//                 reject("Please enter string value");
+//             }
+//         },5000)
+//     })
 
-    const response = await promise;
+//     const response = await promise;
 
-    return response;
+//     return response;
+// }
+
+
+// testData(21).then(data => console.log(data))
+// .catch(err => console.log(err));
+
+
+async function getCurrency(url) {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return data;
+
 }
 
-
-testData(21).then(data => console.log(data))
-.catch(err => console.log(err));
+getCurrency("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oxBwKqWWamYRQhQEDQYNlLhaaTwwjfLWAoX2nL3c")
+.then(data => console.log(data.data.TRY));
