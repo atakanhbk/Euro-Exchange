e1f79f876614f9ebef8ff6865863165dc1971fe4
atakanhbk <atakanhbk@gmail.com>
2023-09-28 15:10:04 +0300

    Fetch API part 1

---
+++ FetchAPI/app.js
@@ -0,0 +1,8 @@
+function getTextFile() {
+    fetch("example.txt")
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+getTextFile();
