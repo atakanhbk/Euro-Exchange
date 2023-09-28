01008655016468b6cb00639a0102e1c7a082c851
atakanhbk <atakanhbk@gmail.com>
2023-09-28 15:37:11 +0300

    Fetch Part 2

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -5,4 +5,20 @@ function getTextFile() {
     .catch(err => console.log(err));
 }
 
-getTextFile();
+function getJsonFile() {
+    fetch("example.json")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
+
+
+function getExternalAPI() {
+    fetch("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oxBwKqWWamYRQhQEDQYNlLhaaTwwjfLWAoX2nL3c")
+    .then(response => response.json())
+    .then(data => console.log(data.data.TRY))
+    .catch(err => console.log(err));
+}
+
+getExternalAPI();
+//getJsonFile ();
